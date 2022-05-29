a255d1d340a94ce7f9564aa2591ea5b3faecebcc
ALI11IB <ali.la.ibrahem@gmail.com>
2022-05-29 16:43:39 +0300

    add packages next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
+const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
+const useSass = withCSS(
+  withSass({
+    reactStrictMode: true,
+
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 100000,
+          },
+        },
+      });
+
+      return config;
+    },
+  })
+);
 const nextConfig = {
   reactStrictMode: true,
-}
+  useSass,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
